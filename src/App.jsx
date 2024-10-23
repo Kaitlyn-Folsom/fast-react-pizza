aa05f7eecc9680ec4f077514fab51e4bf209026d
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-10-22 20:42:38 -0400

    pull order data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Home from './ui/Home';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
-import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
 import AppLayout from './ui/AppLayout';
+import Home from './ui/Home';
+import Cart from './features/cart/Cart';
 import Error from './ui/Error';
+import CreateOrder from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 
 const router = createBrowserRouter([
   {
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
       { path: '/menu', element: <Menu />, loader: menuLoader, errorElement: <Error /> },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
-      { path: '/order/:orderid', element: <Order /> },
+      { path: '/order/:orderId', element: <Order />, loader: orderLoader, errorElement: <Error /> },
     ],
   },
 ]);
