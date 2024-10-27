67816b90f1f4a780d9569947a6cf7f6e6444f005
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-10-27 08:57:41 -0400

    setup redux store & finish user state functionality

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,6 +3,7 @@ import { Form, redirect } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import { useActionData, useNavigation } from "react-router";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -37,6 +38,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+  const username = useSelector((state) => state.user.username);
 
   const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
@@ -51,7 +53,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
