f2d5decca1d50952b9e110154e47b77361687fda
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-11-02 09:04:58 -0400

    setup delete functionality

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/prop-types */
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice.js";
+import DeleteItem from "../cart/DeleteItem.jsx";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     const newPizza = {
@@ -40,7 +44,9 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut && (
+          {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to Cart
             </Button>
